e8bc0b19f3cf534e8e08b5ec553ea66b22428d51
Lee <dan8792@gmail.com>
2022-10-30 00:25:04 +0900

    conn. my sql

--- client/src/App.js
+++ client/src/App.js
@@ -7,51 +7,37 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-// import { withStyles } from '@material-ui/Core/Styles';
+import { withStyles } from '@mui/styles';
+import CircularProgress from '@mui/material/CircularProgress';
 
-// const styles = theme => ({
-//   root: {
-//     width: '100%',
-//     marginTop: theme.spacing.unit * 3,
-//     overflowX: "auto"
-//   },
-//   table: {
-//     minWidth: 1000
-//   }
-// })
 
-// const customers =[{
-//   'id' : 1,
-//   'image' : 'https://placeimg.com/64/64/1',
-//   'name' : '홍길동',
-//   'birthday' : '961222',
-//   'gender':'남자',
-//   'job' : '대학생'
-// },
-// {
-//   'id' : 2,
-//   'image' : 'https://placeimg.com/64/64/2',
-//   'name' : '나이스',
-//   'birthday' : '956621',
-//   'gender':'남자',
-//   'job' : '프로그래머'
-// },
-// {
-//   'id' : 3,
-//   'image' : 'https://placeimg.com/64/64/3',
-//   'name' : '이순신',
-//   'birthday' : '853325',
-//   'gender':'남자',
-//   'job' : '무직'
-// }]
+const styles =  ({
+  root: {
+    width: '100%',
+    // marginTop: theme.spacing.unit * 3,
+    marginTop: 50,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1000
+  },
+  progress: {
+    //margin: theme.spacing.unit * 2
+    marginTop: 50
+  }
+})
+
 
 class App extends Component{
 
+  //값이 변경되는 변수
   state = {
-    customers: ""
+    customers: "",
+    completed : 0
   }
 
   componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
@@ -61,15 +47,21 @@ class App extends Component{
       const response = await fetch('/api/customers');
       const body = await response.json();
       return body;
-    }
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100? 0 : completed + 1});
+  }
+
 
   render(){
-    // const { classes } = this.props;
+     const { classes } = this.props;
     return(
-      // <Paper className={classes.root}>
-        <Paper>
-          {/* <Table className = {classes.table}> */}
-          <Table>
+       <Paper className={classes.root}>
+        {/* <Paper> */}
+          <Table className = {classes.table}>
+          {/* <Table> */}
             <TableHead>
               <TableRow>
                 <TableCell>번호</TableCell>
@@ -92,7 +84,13 @@ class App extends Component{
               gender={c.gender}
               job ={c.job}
               />);
-              }) : ""}
+              }) : 
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress className={classes.progress} varient="determinate" value={this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
             
             </TableBody>
           </Table>
@@ -101,5 +99,8 @@ class App extends Component{
   }
 }
 
-// export default withStyles(styles)(App);
-export default App;
+// export default App;
+
+export default withStyles(styles)(App);
+
+
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParse = require('body-parser');
 const app = express(); 
 // const cors = require('cors');
 const port = process.env.PORT || 5000; 
+const fs = require('fs');
 
 // const corsOptions = {
 //   origin: 'http://localhost:3000',
@@ -17,34 +18,29 @@ app.use(bodyParse.urlencoded({extended: true}));
 //     res.send({message: 'Hello Express!'});
 // });
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host : conf.host,
+  user: conf.user,
+  password: conf.password,
+  port : conf.port,
+  database: conf.database
+});
+connection.connect(); 
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-        'id' : 1,
-        'image' : 'https://placeimg.com/64/64/1',
-        'name' : '홍길동',
-        'birthday' : '961222',
-        'gender':'남자',
-        'job' : '대학생'
-      },
-      {
-        'id' : 2,
-        'image' : 'https://placeimg.com/64/64/2',
-        'name' : '나이스',
-        'birthday' : '956621',
-        'gender':'남자',
-        'job' : '프로그래머'
-      },
-      {
-        'id' : 3,
-        'image' : 'https://placeimg.com/64/64/3',
-        'name' : '이순신',
-        'birthday' : '853325',
-        'gender':'남자',
-        'job' : '무직'
-      }
-    ]);
-})
+   
+      connection.query(
+        "SELECT * FROM crawl_data.CUSTOMER",
+        (err, rows, fields) => {
+          res.send(rows);
+        }
+      );
+    
+});
 
 
 
